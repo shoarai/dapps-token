849c2be3d0755cd7435b4003038547f96c56e16c
shoarai <shoarai@users.noreply.github.com>
2018-04-22 16:51:01 +0900

    Add ropsten network config

--- truffle.js
+++ truffle.js
@@ -1,9 +1,25 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = $MNEMONIC;
+var accessToken = $INFURA_ACCESS_TOKEN;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  development: {
-    host: "localhost",
-    port: 8545,
-    network_id: "10"
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "10"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/" + accessToken
+        );
+      },
+      network_id: 3,
+      gas: 500000
+    }
   }
 };
