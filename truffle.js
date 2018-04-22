469f09d834283601ef14919c1e111552c6439d6a
shoarai <shoarai@users.noreply.github.com>
2018-04-22 16:14:49 +0900

    Add geth network config

--- truffle.js
+++ truffle.js
@@ -1,4 +1,9 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  development: {
+    host: "localhost",
+    port: 8545,
+    network_id: "10"
+  }
 };
